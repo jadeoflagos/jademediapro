5508ba097e7963f3a7a725e8298e722858f87dc6
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2021-12-09 01:49:48 +0100

    WIP: case-study

--- pages/case-study.jsx
+++ pages/case-study.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Image from "next/image";
+import Footer from "../components/Footer";
 
 const CaseStudy = () => {
   return (
     <div>
-      <Navbar dropDownBg="black" bg="#fff" textColor="#000" darkLogo />
+      <div>
+        <Navbar dropDownBg="black" bg="#fff" textColor="#000" darkLogo />
+      </div>
       <Image
         // layout="fill"
         src="/images/awotele.png"
         width={1920}
         height={1027}
       />
+      <main>
+        <div className="header text-center mt-[11rem]">
+          <h2 className="text-[4.5rem] font-semibold">Awotele</h2>
+          <span className="text-[3rem]">Case Study</span>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
