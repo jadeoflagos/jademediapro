3c622733ad3d8a7bc748206929fb687d1b03b4c8
aishacodes <aishatabdulfatah81@gmail.com>
2022-01-15 10:54:59 +0100

    update

--- data/Faq.js
+++ data/Faq.js
@@ -5,47 +5,63 @@ export const faqs = [
   },
   {
     que: "Will I own the design files and animation when it is finished?",
-    ans: "",
+    ans: "Yes, you will. The design files and animation become yours and you get to use them in whatever way you want.",
   },
   {
     que: "Will you keep my work confidential?",
-    ans: "",
+    ans: "Yes! We make sure to keep your work hidden in the confines of our archives until you want it released to the public because once it gets released, it will go viral using our marketing channels.",
   },
   {
     que: "I have a challenge making payment?",
-    ans: "",
+    ans: "We usually do not have this challenge. If it however occurs, the common problem with verification is when credit cards addresses are mismatched. If you have further questions, do well to contact us.",
   },
   {
     que: "Can I change the frequency of my email subscription?",
-    ans: "",
+    ans: "Because we understand that some of our clients get very busy and lack the time to scan through blogs or social streams, we made provision by making our Newsletter give a twice-weekly summary. If you however want more frequent emails, contact us at solutions@jademediapro.com ",
   },
   {
     que: "How can I be sure I’ll like what you design?",
-    ans: "",
+    ans: "A lot of details and information collection is required when branding is concerned and since we will be discussing with you, your business history, goals, competitors, and more, parameters and guide will be set as regards the design process so you can be sure you would love your design.",
   },
   {
     que: "We’re not in the same city. Can we still work together?",
-    ans: "",
+    ans: "Definitely! Distance is no barrier. A large number of our clients aren't local so we are well-experienced in managing long-distance projects. We do this via phone calls, zoom meetings, share screens, google meet, email pdf proofs, recorded video presentations, and mail colour swatches and samples. No stress at all.",
   },
   {
     que: "I need a logo and I already know exactly what I want it to look like. I just need someone to make it. Will you help?",
-    ans: "",
+    ans: "What you need in this case is a skilled designer in bringing your idea to life by the use of specific design strategy. We specialize in creative and strategic thinking and project execution. We can deliver what you need and more.",
   },
   {
     que: "I already have our brand identity established. Can I hire you for a smaller project like a social media post or a single ad? Also, we need it ASAP.",
-    ans: "",
+    ans: "What you need is a designer who specializes in production work and is skilled in the application of an existing brand to a piece of collateral. Yes! We can help you with that.",
   },
   {
     que: "How long does it take to complete a branding project?",
-    ans: "",
+    ans: "Normally, it takes a week for a logo project to about 4 weeks for a standard brand identity.",
+  },
+  {
+    que:"Can you review my logo/brand identity/website/etc.?",
+    ans:"Sure! We would love to. Please contact us at solutions@jademediapro.com for more enquiry on that.",
   },
   {
     que: "Can you handle project management for my company with minimum supervision?",
-    ans: "",
+    ans: "Absolutely Yes, we are PMP certified with over 3 years of experience handling numerous projects remotely, with little/no supervision. We guarantee a great level of success in the projects that we undertake.",
   },
   {
     que: "How does my business benefit from Digital Marketing?",
-    ans: "",
+    ans: "With digital marketing, your product, service, and brand can benefit by gaining massive exposure and positioning of your offer to people who are interested in it and are willing to make a purchasing decision.",
+  },
+  {
+    que:"Do I need a Blog?",
+    ans:"Blogs are essential to digital marketing as they are an easier way to stay in front of your target market. When we help you publish your blogs, you’re lending your voice on industry topics while also proffering solutions to common problems using. Having a blog on your site would be crucial in terms of driving unique traffic to your website."
+  },
+  {
+    que:"Is Email Marketing still effective?",
+    ans:"As a matter of fact, email marketing is more effective than ever. Arguably, it is even more popular than any social media. This is because it is more private, more personal, and more reliable. Many people rely on email and check their inbox several times daily. A lot of companies have recorded a considerable increase in sales when they utilize email marketing to sell their offers. So, if you want to connect directly with people who permit you to market to them, email is the most essential tool to use."
+  },
+  {
+    que:"",
+    ans:""
   },
   {
     que: "Does the water bowl act to purify the hookah aerosol? ?",
