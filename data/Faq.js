48d7949d6c501b1dafe42d4b472e6380f95f75df
aishacodes <aishatabdulfatah81@gmail.com>
2022-01-15 11:52:30 +0100

    faq content

--- data/Faq.js
+++ data/Faq.js
@@ -60,22 +60,115 @@ export const faqs = [
     ans:"As a matter of fact, email marketing is more effective than ever. Arguably, it is even more popular than any social media. This is because it is more private, more personal, and more reliable. Many people rely on email and check their inbox several times daily. A lot of companies have recorded a considerable increase in sales when they utilize email marketing to sell their offers. So, if you want to connect directly with people who permit you to market to them, email is the most essential tool to use."
   },
   {
-    que:"",
-    ans:""
+    que:"What I can expect on our first call?",
+    ans:"The first step of starting our strategy process is a 30-minute discovery call. In this call, we’ll try to clarify your goals, perform problem discovery and identify a potential solution to your problem. After our call we’ll give you a proposal for the discovery session, so you can approve and set a down payment. We’ll be able to create and deliver a final project proposal, and then we can formally start our project."
   },
   {
-    que: "Does the water bowl act to purify the hookah aerosol? ?",
-    ans: `                No. All tobacco products have risks but comparing hookah to
-                  cigarettes is erroneous and misleading. Hookah is heated at a
-                  temperature that is four times cooler than a burning cigarette
-                  and creates an aerosol that is 75% water (60%) and glycerol
-                  (15%). By contrast, cigarette smoke is 75% tar, carbon monoxide,
-                  and nicotine . Most people enjoy hookah in moderation, far less
-                  often than typical smokers light up a cigarette. In the United
-                  States, for example, 90% of people who enjoy hookah do so no`,
+    que:"Will my service start immediately?",
+    ans:"Your service will start immediately after payment confirmation."
   },
   {
-    que: "Is a one-hour hookah session the same as smoking 100 or 200 cigarettes ?",
-    ans: "",
+    que:"If I need support, how can I reach you?",
+    ans:"You can reach us 24/7 by email. Phone and chat support is 8 AM to 7 PM Mondays – Saturdays. We are always glad to assist. Contact us at solutions@jademediapro.com"
   },
+  {
+    que:"Do I need an account for making a purchase?",
+    ans:"No, it’s not necessary to create an account before purchasing on the site but it is highly recommended, since it has its advantages, such as tracking the delivery status of your order(s), you can view the entire history of your past orders, make a plan for future orders through useful features like Wishlist and Notify Me, you can write reviews for products bought from us, you can subscribe to our weekly newsletter."
+  },
+  {
+    que:"How do I place an order?",
+    ans:"Placing an order is quick and simple. Visit products on our store, as you browse through, you can add the items you wish to buy in your Shopping Cart by clicking on ‘Add to Cart’. Log in with your email address and password, enter your shipping address and make the payment."
+  },
+  {
+    que:"How long does the delivery process take?",
+    ans:"We ship out your items from our office 48 hours (2 working days) after payment is done. For Lagos deliveries, Delivery is done the same or next day. However, for outside Lagos orders, delivery is done 3-5days after payment confirmation. International orders take a bit longer depending on your location."
+  },
+  {
+    que:"Can I get a refund on my hosting fee?",
+    ans:"Unfortunately, we do not offer refunds on our web hosting accounts for any reason."
+  },
+  {
+    que: "Do you register domain names?",
+    ans:"Yes, we do. We’ll be more than happy to register a new domain."
+  },
+  {
+    que: "Do you provide website hosting?",
+    ans: "Yes, we do. If you don’t already have a host, we’ve worked with several hosting providers and can give you some recommendations if don’t have a hosting provider already.",
+  },
+  {
+    que:"Can I see how many visitors I have to my website?",
+    ans:"Yes, you can. We can set up a free Google Analytics account for you, which will automatically record information about your website’s visitors."
+  },
+  {
+    que:"How many revisions can I make to the design project?",
+    ans:"We try to make the most informed decisions when it comes to creating a design, which is why we get to know you, your market, and your audience before we prepare mock-ups. At that point, we ask that there be no more than one (1) major revision to the design. Naturally, you’ll have smaller changes and additions, but at that point, we consider the overall structure and design to be set. If you request too many changes to your project, or if you request changes to the project after the entire strategy has been formatted with the template, you will incur more charges."
+  },
+  {
+    que:"Do you provide mock-ups?",
+    ans:"Yes, we do. We provide full-colour mock-ups to our clients for every project we work on once a contract has been signed. These mock-ups are the first step in our design process. Once we show them to you and come to an agreement about the design, they’ll be the basis for your new branding or product development."
+  },
+  {
+    que:"Do I own my website?",
+    ans:"YES! Everything that we build will be 100% owned by you. Most of our clients stay with us for the life of the website. Our team is just irresistible that way. But if for any reason you decide you want to take your site to another hosting service and get another company to service the site, we will happily assist you in making the transition as effortlessly and efficiently as possible. The website is yours after all, and we want you to take it wherever you go."
+  },
+  {
+    que:"Can you help me update the existing website that another web firm built? ",
+    ans:"Yes. We support websites that we have fully built on Content Management System."
+  },
+  {
+    que:"Who will be communicating and working me through during my project?",
+    ans:"Our clients work directly with our tight-knit team of 5. Who that will be depend on what your project needs and what stage of the process you are in during the project."
+  },
+  {
+    que:"How long does it take to build a website?",
+    ans:"he time always vary from project to project. Our standard websites take approximately 15 days to create. Our E-commerce (online store) websites take approximately 30 days to create."
+  },
+  {
+    que:"Do you require a deposit? or What are your payment steps?",
+    ans:"Yes, we do. We ask our clients for a 70% deposit on all projects, to be paid before work begins on the project. After your final approval on the project, you need to pay the remaining 30% to enable us to transfer the whole document to you."
+  },
+  {
+    que:"What is your refund policy?",
+    ans:"By policy, we don't accept refund requests on the sale of digital goods unless it's stated clearly on a published sales page. You can check www.jademediapro.com/return-policy for more information."
+  },
+  {
+    que:"Can you set up and manage our Facebook ad campaign?",
+    ans:"Yes, we can. We will need to discuss with you to determine your campaign objective and come up with a strategy that fits you."
+  },
+  {
+    que:"I don't need all the services; can I pick and choose?",
+    ans:"Yes, just contact us and we'll guide you through by taking advantage of our 30 mins free consultation to create you a custom order with the services you need."
+  },
+  {
+    que:"How does photography enhance advertising my product/service?",
+    ans:"Everything boils down to having more sales, and successful photographs help your customers have a clear view of what you offer, so if they are convinced enough by your offers, they will make a purchase."
+  },
+  {
+    que:"Why is Branding Important for my brand-new company?",
+    ans:"Branding is a way of differentiating your company from other companies. It is important to clarify what you represent and what your offers are as opposed to your competitors, so it is important because it helps to create a long-lasting impression on your client."
+  },
+  {
+    que:"Can graphic design help me with advertising my product/service?",
+    ans:`The graphic is one of the key elements that help us bring out advertising. It plays a crucial role in advertising. The word graphic itself screams "advertising".`
+  },
+  {
+    que:"Why is business strategy important for my company? ",
+    ans:"Without a business strategy, your business has no future. A business strategy is what helps you create a vision and direction for your organization. It helps you create goals and come up with ideas on how to crush them."
+  },
+  {
+    que:"What is the best business strategy for my new business?",
+    ans:"To create more exposure for your business, make use of social media platforms like Facebook ads, Google My Business, content marketing, webinars, etc. You can also incorporate our free consultation technique to boost your customer base.    "
+  },
+  {
+    que:"Why is digital marketing important for my brand-new company?",
+    ans:"Digital marketing for a new business is very necessary because it helps create exposure and, by extension, more sales. It also helps customers' communication with business owners."
+  },
+  {
+    que:"What is the importance of motion design to my business?",
+    ans:"As humans, we are constantly viewing content, and we only pay attention to the ones that are compelling or catchy. So, to capture the attention of your audience, you need to visually communicate your message more compellingly, and this is where motion design comes in."
+  },
+  {
+    que:"What is the difference between a mobile app and a mobile website?",
+    ans:"A mobile app is a program that is downloaded and installed onto a user's mobile device, while a mobile website is primarily accessed via the internet, that is, it requires you to be online."
+  }
 ];
