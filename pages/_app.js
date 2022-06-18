303d6d9b813d9def1f3f5133c239e79529e0367b
aishacodes <aishatabdulfatah81@gmail.com>
2022-06-18 12:56:51 +0100

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import Head from "next/head";
 const theme = createTheme({
   palette: {
     primary: {
@@ -12,6 +12,9 @@ const theme = createTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
